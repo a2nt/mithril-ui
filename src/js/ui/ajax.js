ec35f183796855cf0b01f55e87644632eacb0f04
Tony Air <tony@twma.pro>
2024-06-12 20:02:51 +0200

    IMPR: uncached AJAX request

--- src/js/ui/ajax.js
+++ src/js/ui/ajax.js
@@ -1,35 +1,35 @@
 const NAME = 'uiAjax'
 
 const m = require("mithril") // eslint-disable-line
-//const curParams = [...new URLSearchParams(window.location.search).entries()].reduce((r, [k, v]) => { r[k] = v; return r; }, {})
+// const curParams = [...new URLSearchParams(window.location.search).entries()].reduce((r, [k, v]) => { r[k] = v; return r; }, {})
 const options = {
-    withCredentials: true,
-    params: {
-        //...curParams,
-        ajax: '1',
-        m: Date.now(),
-    },
-    headers: {
-        HTTP_X_REQUESTED_WITH: 'XMLHttpRequest'
-    }
+  withCredentials: true,
+  params: {
+    // ...curParams,
+    ajax: '1',
+    m: Date.now()
+  },
+  headers: {
+    HTTP_X_REQUESTED_WITH: 'XMLHttpRequest'
+  }
 }
 
 const AJAX = {
-    request: async (opts) => {
-        const res = await m.request({
-            ...options,
-            ...opts,
-        })
+  request: async (opts) => {
+    const res = await m.request({
+      ...options,
+      ...opts
+    })
 
-        if (res && res.errors) {
-            console.error(`${NAME} > request: Error`)
-            res.errors.forEach((e) => {
-                console.error(e.message)
-            })
-        }
-
-        return await res
+    if (res && res.errors) {
+      console.error(`${NAME} > request: Error`)
+      res.errors.forEach((e) => {
+        console.error(e.message)
+      })
     }
+
+    return await res
+  }
 }
 
 module.exports = AJAX
