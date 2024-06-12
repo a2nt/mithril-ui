dffcd0fcfb5d0ac4c8a6df6481b5dff282142e56
Tony Air <tony@twma.pro>
2024-06-12 20:02:10 +0200

    IMPR: uncached AJAX request

--- src/js/ui/ajax.js
+++ src/js/ui/ajax.js
@@ -1,32 +1,35 @@
 const NAME = 'uiAjax'
 
 const m = require("mithril") // eslint-disable-line
+//const curParams = [...new URLSearchParams(window.location.search).entries()].reduce((r, [k, v]) => { r[k] = v; return r; }, {})
 const options = {
-  withCredentials: true,
-  params: {
-    ajax: '1'
-  },
-  headers: {
-    HTTP_X_REQUESTED_WITH: 'XMLHttpRequest'
-  }
+    withCredentials: true,
+    params: {
+        //...curParams,
+        ajax: '1',
+        m: Date.now(),
+    },
+    headers: {
+        HTTP_X_REQUESTED_WITH: 'XMLHttpRequest'
+    }
 }
 
 const AJAX = {
-  request: async (opts) => {
-    const res = await m.request({
-      ...options,
-      ...opts
-    })
+    request: async (opts) => {
+        const res = await m.request({
+            ...options,
+            ...opts,
+        })
 
-    if (res && res.errors) {
-      console.error(`${NAME} > request: Error`)
-      res.errors.forEach((e) => {
-        console.error(e.message)
-      })
+        if (res && res.errors) {
+            console.error(`${NAME} > request: Error`)
+            res.errors.forEach((e) => {
+                console.error(e.message)
+            })
+        }
+
+        return await res
     }
-
-    return await res
-  }
 }
 
 module.exports = AJAX
