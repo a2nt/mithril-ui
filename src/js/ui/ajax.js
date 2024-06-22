b14d1bc51dcf5190dfcec5da39200b4f40edb73d
Tony Air <tony@twma.pro>
2024-06-22 22:53:37 +0200

    IMPR: Allow to preload content

--- src/js/ui/ajax.js
+++ src/js/ui/ajax.js
@@ -3,33 +3,39 @@ const NAME = 'uiAjax'
 const m = require("mithril") // eslint-disable-line
 // const curParams = [...new URLSearchParams(window.location.search).entries()].reduce((r, [k, v]) => { r[k] = v; return r; }, {})
 const options = {
-  withCredentials: true,
-  params: {
-    // ...curParams,
-    ajax: '1',
-    m: Date.now()
-  },
-  headers: {
-    HTTP_X_REQUESTED_WITH: 'XMLHttpRequest'
-  }
+    withCredentials: true,
+    params: {
+        // ...curParams,
+    },
+    headers: {
+        HTTP_X_REQUESTED_WITH: 'XMLHttpRequest'
+    }
 }
 
 const AJAX = {
-  request: async (opts) => {
-    const res = await m.request({
-      ...options,
-      ...opts
-    })
+    request: async (opts) => {
+        if (!opts.url.includes('ajax=1')) {
+            options.params.ajax = '1'
+        }
 
-    if (res && res.errors) {
-      console.error(`${NAME} > request: Error`)
-      res.errors.forEach((e) => {
-        console.error(e.message)
-      })
+        if (!opts.url.includes('m=')) {
+            options.params.m = Date.now()
+        }
+
+        const res = await m.request({
+            ...options,
+            ...opts
+        })
+
+        if (res && res.errors) {
+            console.error(`${NAME} > request: Error`)
+            res.errors.forEach((e) => {
+                console.error(e.message)
+            })
+        }
+
+        return await res
     }
-
-    return await res
-  }
 }
 
 module.exports = AJAX
